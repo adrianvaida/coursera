7d141c2638619d9a919a04fb872ed0c9ad3601cb
Adrian Vaida <ady.viada@yahoo.com>
2017-09-27 09:57:29 +0300

    primu quiz

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+var quiz = [
+	["What is Superman's real name?", "Clarke Kent"],
+	["What is Wonderwoman's real name?", "Diana Prince"],
+	["What is Batman's real name?", "Bruce Wayne"]
+];
+
+var score = 0; // initial score
+
+play(quiz);
+
+function play(quiz) {
+	// main game loop
+	for(var i = 0, question, answer, max = quiz.length; i < max; i++) {
+		question = quiz[i][0];
+		answer = ask(question);
+		check(answer);
+	}
+	// end of main game loop
+	gameOver();
+	
+	function ask(question) {
+		return prompt(question); // quiz[i][0] is the ith question
+	}
+	function check(answer) {
+		if(answer === quiz[i][1]) { // quiz[i][1] is the ith answer
+			alert("Correct!");
+			// increase score by 1
+			score++;
+		} else {
+			alert("Wrong!");
+		}
+	}
+	function gameOver() {
+		// inform the player that the game has finished and tell how many points they have scored
+		alert("Game Over, you scored " + score + " points");
+	}
+}
